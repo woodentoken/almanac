6319a1675bc417e0ca8e80d69f975ae9f94fcd71
Kaleb Bordner <kaleb.bordner@gmail.com>
2025-08-06 21:11:11 -0400

    fixing splash screen, configuring series grid;

--- static/js/splash.js
+++ static/js/splash.js
@@ -1,62 +1,50 @@
-//Initializing
-var images = []; //rray
-var i = 0; // Index for the current image
+var images = [];
+var i = 0;
 var time = 10000;
 
 images[0] = "https://ik.imagekit.io/ry1ze0vkn/splash/crane.jpg?tr=lqip";
 images[1] = "https://ik.imagekit.io/ry1ze0vkn/splash/heron.jpg?tr=lqip";
-images[2] = "https://ik.imagekit.io/ry1ze0vkn/splash/fog.jpg?tr=lqip";
-images[3] = "https://ik.imagekit.io/ry1ze0vkn/splash/palm.jpg?tr=lqip";
-images[4] = "https://ik.imagekit.io/ry1ze0vkn/splash/water.jpg?tr=lqip";
+images[2] = "https://ik.imagekit.io/ry1ze0vkn/splash/palm.jpg?tr=lqip";
+images[3] = "https://ik.imagekit.io/ry1ze0vkn/splash/water.jpg?tr=rt-180,lqip";
 
 document.addEventListener('DOMContentLoaded', function() {
   const el = document.getElementById('splash');
-  randomIndex = Math.floor(Math.random() * images.length);
-  el.style.backgroundImage = images[randomIndex]; /            <a href="{{ url_for('series') }}">series</a>            <a href="{{ url_for('series') }}">series</a>/ Initial image
- });
-
+  const randomIndex = Math.floor(Math.random() * images.length);
+  el.style.backgroundImage = `url("${images[randomIndex]}")`;
+});
 
 // Preload images
 function preloadImages() {
-    var imageUrls = [
-        "https://ik.imagekit.io/ry1ze0vkn/splash/crane.jpg?tr=lqip",
-    ];
-    
-    var loadedCount = 0;
-    var totalImages = imageUrls.length;
-    
-    imageUrls.forEach(function(url) {
-        var img = new Image();
-        img.src = url;
-    console.log('Preloading image:', url);
-        img.onload = function() {
-            loadedCount++;
-            if (loadedCount === totalImages) {
+  var loadedCount = 0;
+  var totalImages = images.length;
+
+  images.forEach(function(url) {
+    var img = new Image();
+    img.onload = function() {
+      loadedCount++;
+      if (loadedCount === totalImages) {
         console.log('All images preloaded successfully.');
-                changeImage(); // Start slideshow after all images are loaded
-            }
-        };
-        img.onerror = function() {
-            console.warn('Failed to load image:', url);
-            loadedCount++;
-            if (loadedCount === totalImages) {
-                changeImage(); // Start slideshow even if some images failed
-            }
-        };
-        img.src = url;
-    });
+        changeImage();
+      }
+    };
+    img.onerror = function() {
+      console.warn('Failed to load image:', url);
+      loadedCount++;
+      if (loadedCount === totalImages) {
+        changeImage();
+      }
+    };
+    img.src = url;
+  });
 }
 
 function changeImage() {
-    var el = document.getElementById('splash');
+  var el = document.getElementById('splash');
+  el.style.backgroundImage = `url("${images[i]}")`;
   console.log('Changing image to:', images[i]);
-    el.style.backgroundImage = images[i];
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-    setTimeout(changeImage, time); // Also fixed the setTimeout syntax
+  i = (i + 1) % images.length;
+  setTimeout(changeImage, time);
 }
 
 window.onload = preloadImages;
+
