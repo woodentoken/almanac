31bc7ce76d57bd962a097749724529515fcf8ad1
Kaleb Bordner <kaleb.bordner@gmail.com>
2025-08-24 19:40:59 -0400

    adding dark mode toggle on icon

---
+++ static/js/light_dark.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const toggle = document.getElementById('toggle');
+  const body = document.body;
+  const currentTheme = localStorage.getItem('theme');
+
+  // Load preference
+  if (localStorage.getItem("theme") === "dark-mode") {
+    body.classList.add("dark-mode");
+  }
+   else {
+    body.classList.add("light-mode");
+  }
+
+  toggle.addEventListener('click', function() {
+    if (body.classList.contains('light-mode')) {
+      // toggle.src = "static/logo/almanac_logo_white.svg";
+      body.classList.replace('light-mode', 'dark-mode');
+      localStorage.setItem('theme', 'dark-mode');
+    } else {
+      // toggle.src = "static/logo/almanac_logo.svg";
+      body.classList.replace('dark-mode', 'light-mode');
+      localStorage.setItem('theme', 'light-mode');
+    }
+  });
+});
