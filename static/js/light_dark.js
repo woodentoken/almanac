0684834a5e313995df3a4eb52243eb2a9c5bcffc
Kaleb Bordner <kaleb.bordner@gmail.com>
2025-08-24 22:30:21 -0400

    toggling document level colorscheme

--- static/js/light_dark.js
+++ static/js/light_dark.js
@@ -5,7 +5,7 @@
     if (theme === 'dark-mode' || (!theme && window.matchMedia('(prefers-color-scheme: dark-mode)').matches)) {
       document.documentElement.classList.add('dark-mode');
     } else {
-      document.documentElement.classList.remove('dark-mode');
+      document.documentElement.classList.replace('dark-mode', 'light-mode');
     }
   } catch (e) {}
 })();
@@ -24,7 +24,19 @@ document.addEventListener('DOMContentLoaded', function() {
     body.classList.add("light-mode");
   }
 
+
   toggle.addEventListener('click', function() {
+
+    // ensures the document follows the color scheme changes
+    if (document.documentElement.classList.contains('dark-mode')) {
+      document.documentElement.classList.replace('dark-mode', 'light-mode');
+      // localStorage.setItem('theme', 'light-mode');
+    } else {
+      document.documentElement.classList.replace('light-mode', 'dark-mode');
+      // localStorage.setItem('theme', 'dark-mode');
+    }
+
+
     if (body.classList.contains('light-mode')) {
       // toggle.src = "static/logo/almanac_logo_white.svg";
       body.classList.replace('light-mode', 'dark-mode');
